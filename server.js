6fc4a627410d7bf57042ec46c243ccde577e95fa
Charmila-eti <charmilaeti@gmail.com>
2026-06-12 22:21:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -78,8 +78,7 @@ app.get('/api/backup', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT} 🔥`);
+  console.log(`Server running on port ${PORT}`);
 });
